25ffccd221d10b4b020e2c6be0f0b62be94988ca
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-16 23:25:29 -0400

    getting express to run

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,11 @@
+// const express = require("express");
+// const app = express();
+
+// app.listen(1234, () => {
+//   console.log("Server is listening on port: 1234");
+// });
+
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
