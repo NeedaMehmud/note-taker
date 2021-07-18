1b49da5ea0ec4555f8bcc164e170bdc449a96ba6
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-18 00:44:19 -0400

    got the delete button working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 // 'uuid' for unique id
 const { v4: uuidv4 } = require('uuid');
-const data = require("../db/db.json");
+let data = require("../db/db.json");
 
 // routing api get request
 module.exports = (server) => {
@@ -29,10 +29,17 @@ module.exports = (server) => {
 
 
     server.delete("/api/notes/:id", (request, response) => {
-        let noteId = request.params.id.toString();
+        let noteId = request.params.id.toString().replace(":", "");
         console.log(`\n\nDELETE note request for noteId: ${noteId}`);
+        let newData = [];
+        for(let i = 0; i < data.length; i++) {
+            if(data[i].id != noteId) {
+                newData.push(data[i]);
+            }
+        }
         fs.writeFileSync('./db/db.json', JSON.stringify(newData));
         console.log(`\nSuccessfully deleted note with id : ${noteId}`);
+        data = newData;
         response.json(newData);
     });
 };
